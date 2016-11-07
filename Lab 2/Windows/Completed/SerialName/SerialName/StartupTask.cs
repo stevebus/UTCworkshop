5a57c2f22335706438eaf0806626969871d86e1c
Steve Busby <stevebus@microsoft.com>
2016-11-07 09:38:36 -0600

    initial commit

---
+++ Lab 2/Windows/Completed/SerialName/SerialName/StartupTask.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.SerialCommunication;
+using Windows.Devices.Enumeration;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace SerialName
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+
+        private ObservableCollection<DeviceInformation> listOfDevices = new ObservableCollection<DeviceInformation>();
+        private BackgroundTaskDeferral deferral;
+
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            deferral = taskInstance.GetDeferral();
+            ListAvailablePorts().Wait();
+        }
+
+        private async Task ListAvailablePorts()
+        {
+            string aqs = SerialDevice.GetDeviceSelector();
+            System.Diagnostics.Debug.WriteLine(aqs);
+            var dis = await DeviceInformation.FindAllAsync(aqs);
+
+            for (int i = 0; i < dis.Count; i++)
+            {
+                listOfDevices.Add(dis[i]);
+                string t = "Device name:  " + dis[i].Id;
+                System.Diagnostics.Debug.WriteLine(t);
+                t = "Device description:  " + dis[i].Name;
+                System.Diagnostics.Debug.WriteLine(t);
+            }
+        }
+    }
+}
---
+++ Lab 2/Windows/Completed/StudentXXGateway/StudentXXGateway/StartupTask.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Text;
+using Windows.ApplicationModel.Background;
+using Newtonsoft.Json;
+using Microsoft.Azure.Devices.Client;
+using Windows.Devices.SerialCommunication;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using System.Diagnostics;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace StudentXXGateway
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+
+        private BackgroundTaskDeferral deferral;
+
+        static DeviceClient deviceClient;
+        private static string iotHubUri = "<youriothubname>.azure-devices.net";  // <iothubname>.azure-devices.net
+        private string deviceId = "studentXX"; // replace XX with your studentID
+        private static string deviceKey = "<<your device key>>";  // use your key, not mine!  :-)
+
+        // classes related to serial communication
+        static string serialDeviceName = @"<<your serial port name>>";
+        // example serialDeviceName:   \\?\FTDIBUS#VID_0403+PID_6001+AL01GQQHA#0000#{86e0d1e0-8089-11d0-9ce4-08003e301f73}
+        private static SerialDevice serialPort;
+        private static DataReader dataReaderObject = null;
+        private static DataWriter dataWriteObject = null;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // get a deferral token.  this keeps the app from 'exiting' until we want it to
+            deferral = taskInstance.GetDeferral();
+
+            // create the connection to IoTHub, based on the URI, device ID, and key from above
+            deviceClient = DeviceClient.Create(iotHubUri,
+                AuthenticationMethodFactory.
+                    CreateAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey),
+                TransportType.Http1);
+
+            // connect to the serial port
+            SetupSerialConnection().Wait();
+
+            Debug.WriteLine("Setting up Data Reader");
+            // get a pointer to the buffer of data that has been sent to the serial port so we can read it
+            dataReaderObject = new DataReader(serialPort.InputStream);
+
+            Debug.WriteLine("Wiring up Command Receiver...");
+            // start the thread to listen for "commands" from IoThub (i.e. turn our LED on/off)
+            ReceiveCommands();   //.Start();
+
+            // loop forever and receive serial data and sent to IoTHub
+            Debug.WriteLine("Starting receive loop");
+            while (true)
+            {
+                try {
+                    ReadAsync().Wait();
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                }
+            }
+
+        }
+
+        private async Task SetupSerialConnection()
+        {
+            // get a handle to the serial device specified earlier
+            serialPort = await SerialDevice.FromIdAsync(serialDeviceName);
+
+            if (serialPort == null)
+                Debug.WriteLine("Oops - cannot connect to serial port");
+
+            Debug.WriteLine("connected to serial port");
+
+            // Configure serial settings
+            serialPort.WriteTimeout = TimeSpan.FromMilliseconds(1000);
+            serialPort.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+            serialPort.BaudRate = 9600;
+            serialPort.Parity = SerialParity.None;
+            serialPort.StopBits = SerialStopBitCount.One;
+            serialPort.DataBits = 8;
+            serialPort.Handshake = SerialHandshake.None;
+
+            // Display configured settings
+            string status;
+            status = "Serial port configured successfully: ";
+            status += serialPort.BaudRate + "-";
+            status += serialPort.DataBits + "-";
+            status += serialPort.Parity.ToString() + "-";
+            status += serialPort.StopBits;
+            Debug.WriteLine(status);
+
+        }
+
+        private async Task ReadAsync()
+        {
+
+            // for the lab, we have a fixed message length, so we can just read that many bytes at a time
+            // otherwise we have to read a byte at a time and look for newlines
+            uint ReadBufferLength = 13;
+
+            // Set InputStreamOptions to complete the asynchronous read operation when one or more bytes is available
+            dataReaderObject.InputStreamOptions = InputStreamOptions.Partial;
+
+            // load the reader with data from the buffer
+            UInt32 bytesRead = await dataReaderObject.LoadAsync(ReadBufferLength);
+
+            // did we get a message?
+            if (bytesRead > 0)
+            {
+                // get data out of the buffer as a string
+                string x = dataReaderObject.ReadString(bytesRead);
+
+                // chop off the CRLF
+                x = x.Substring(0, x.IndexOf("\r\n"));
+
+                // split the humidity and temp into an array
+                string[] readings = x.Split(',');
+
+                string tempStr = string.Format("Humidity={0}, Temperature={1}", readings[0], readings[1]);
+                System.Diagnostics.Debug.WriteLine(tempStr);
+
+                // create a new telemetryDataPoint object to hold the data
+                var telemetryDataPoint = new
+                {
+                    DeviceId = deviceId,
+                    Temperature = readings[1],
+                    Humidity = readings[0],
+                    ExternalTemperature = 0
+                };
+
+                // serialize the telemetryDataPoint object into JSON
+                string messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+
+                // send the message to IotHub
+                SendDeviceToCloudMessagesAsync(messageString);
+            }
+        }
+        static async void SendDeviceToCloudMessagesAsync(string messageToSend)
+        {
+            // conver message to a byte array and wrap it with IoTHub message metadata
+            var message = new Message(Encoding.ASCII.GetBytes(messageToSend));
+
+            // send the message to IoTHub
+            await deviceClient.SendEventAsync(message);
+
+        }
+        static async Task ReceiveCommands()
+        {
+            Message receivedMessage;
+            string messageData;
+
+            while (true)
+            {
+                // checck for a messages
+                receivedMessage = await deviceClient.ReceiveAsync();
+
+                // did we receive a message?
+                if (receivedMessage != null)
+                {
+                    // unwrap the messages object and get the message string
+                    messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                    string tempStr = String.Format("******Received Command: {0}", messageData);
+                    Debug.WriteLine(tempStr);
+
+                    // tell IoTHub that we got the message (updates the status in the portal)
+                    await deviceClient.CompleteAsync(receivedMessage);
+
+                    // see what the command was and act on it (send to arduino)
+                    if (messageData.Contains("ON"))
+                    {
+                        Debug.WriteLine("Turning LED on");
+                        WriteSerialAsync("ON").Wait();
+                    }
+                    else if (messageData.Contains("OFF"))
+                    {
+                        Debug.WriteLine("Turning LED Off");
+                        WriteSerialAsync("OFF").Wait();
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Unrecognized command - ignoring...");
+                    }
+
+
+                }
+
+                // wait before polling again  (only have to do this if it's HTTP connection)
+                System.Threading.Tasks.Task.Delay(10000).Wait();
+            }
+        }
+
+        static async Task WriteSerialAsync(string Message)
+        {
+
+            // get a handle to the output buffer of the serial port so we can write to it
+            dataWriteObject = new DataWriter(serialPort.OutputStream);
+
+            // write the data and append a newline (arduino uses this to parse)
+            // this line only writes to internal buffer
+            dataWriteObject.WriteString(Message + '\n');
+
+            // write to serial port
+            UInt32 bytesWritten = await dataWriteObject.StoreAsync();
+
+            if (bytesWritten > 0)
+            {
+                string tempStr = Message + " ";
+                tempStr += " written successfully!";
+            }
+
+            // detach our handle to the serial buffer and destroy
+
+            dataWriteObject.DetachStream();
+            dataWriteObject.Dispose();
+
+        }
+
+
+    }
+}
